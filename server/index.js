ca9bab68b2378bf1a8f33bd915ad5cd1f763b9d8
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2019-01-03 13:21:32 -0800

    fix(server): retry starting the server with a different port
    Fixes #31

--- server/index.js
+++ server/index.js
@@ -2,6 +2,18 @@ import app from './app';
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
-  console.log(`Omniboard is listening on port ${PORT}!`);
-});
+const startServer = (port) => {
+  app.listen(port, () => {
+    console.log(`Omniboard is listening on port ${port}!`);
+  }).on('error', (e) => {
+    if (e.code === 'EADDRINUSE') {
+      const newPort = Number(port) + 1;
+      console.log(`Error: Port ${port} is already in use. Retrying with port ${newPort}...`);
+      startServer(newPort);
+    } else {
+      console.error(e);
+    }
+  });
+};
+
+startServer(PORT);
