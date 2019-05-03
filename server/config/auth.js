86bf5a112a9296cd88cff8f86e2d6e9101cb789d
Vojtech Micka <vojtech@nnaisense.com>
2019-05-03 18:02:23 +0200

    Match formating of auth.js

--- server/config/auth.js
+++ server/config/auth.js
@@ -10,23 +10,23 @@ let authPwd = '';
 let authSecret = '';
 
 if ('u' in argv) {
-    if (argv['u'] && argv['u'].indexOf(':')) {
-		// Parse argument in format -u user:pwd:secret
-		const authArray = argv['u'].split(':');
-		if (authArray.length === 3) {
-			authUser = authArray[0];
-			authPwd = authArray[1];
-			authSecret = authArray[2];
-		} else {
-			throw new Error('Invalid command line argument passed to "-u" option.');
-		}
+  if (argv['u'] && argv['u'].indexOf(':')) {
+    // Parse argument in format -u user:pwd:secret
+    const authArray = argv['u'].split(':');
+    if (authArray.length === 3) {
+      authUser = authArray[0];
+      authPwd = authArray[1];
+      authSecret = authArray[2];
     } else {
-		throw new Error('Invalid command line argument passed to "-u" option.');
+      throw new Error('Invalid command line argument passed to "-u" option.');
     }
+  } else {
+    throw new Error('Invalid command line argument passed to "-u" option.');
+  }
 } else {
-    authUser = defaultUser;
-    authPwd = defaultPwd;
-    authSecret = defaultSecret;
+  authUser = defaultUser;
+  authPwd = defaultPwd;
+  authSecret = defaultSecret;
 }
 
 export {authUser, authPwd, authSecret};
