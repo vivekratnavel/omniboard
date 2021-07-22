579070e531359af1956ad5527ee29287d585ecf2
Maximilian Schambach <maximilian.schambach@kit.edu>
2021-07-22 18:10:18 +0200

    Update color palette

--- web/src/components/MetricsPlotView/metricsPlotView.js
+++ web/src/components/MetricsPlotView/metricsPlotView.js
@@ -174,16 +174,16 @@ class MetricsPlotView extends Component {
     }
 
     const colors = [
-      '#1f77b4',
-      '#ff7f0e',
-      '#2ca02c',
-      '#d62728',
-      '#9467bd',
-      '#8c564b',
-      '#e377c2',
-      '#7f7f7f',
-      '#bcbd22',
-      '#17becf'
+      '#0173b2',
+      '#de8f05',
+      '#029e73',
+      '#d55e00',
+      '#cc78bc',
+      '#ca9161',
+      '#fbafe4',
+      '#949494',
+      '#ece133',
+      '#56b4e9'
     ];
 
     const selectedMetricNames = this._getSelectedMetrics(metricNameOptions);
